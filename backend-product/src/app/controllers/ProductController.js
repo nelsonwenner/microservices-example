24032176c4c91f82e5b01c6e1a14364ec792eace
wenner <nelsondiasdemedeiros@gmail.com>
2020-04-25 19:00:57 -0300

    feature: verify stock from product

--- backend-product/src/app/controllers/ProductController.js
+++ backend-product/src/app/controllers/ProductController.js
@@ -7,7 +7,7 @@ class ProductController {
 
         try {
 
-            const attributes = ['product_id', 'product', 'price']
+            const attributes = ['product_id', 'product', 'price', 'stock']
             const products = await Product.findAll({attributes: attributes});
             return res.status(200).json(products);
             
@@ -24,13 +24,35 @@ class ProductController {
         try {
             
             const { id } = req.params;
-            const attributes = ['product_id', 'product', 'price']
+            const attributes = ['product_id', 'product', 'price', stock]
             const product = await Product.findOne({where: {product_id: id}, attributes: attributes});
             return res.status(200).json(product);
             
         } catch (error) {
             switch (error.message) {
                 case error.message:
+                    return res.status(400).json({error: error.message });
+            }
+        }
+    }
+
+    async stock(req, res) {
+
+        try {
+            
+            const { id } = req.params;
+            const attributes = ['product_id', 'product', 'price', stock]
+            const product = await Product.findOne({where: {product_id: id, stock: 1}, attributes: attributes});
+
+            if (!product) { throw 'not in stock' }
+
+            return res.status(200).json({status: true});
+            
+        } catch (error) {
+            switch (error.message) {
+                case 'not in stock':
+                    return res.status(404).json({status: false });
+                default:
                     return res.status(400).json({error: error.message });
             }
         }
