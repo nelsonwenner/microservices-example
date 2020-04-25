98555ee12972793c14fefb569c456760cd55708f
wenner <nelsondiasdemedeiros@gmail.com>
2020-04-25 18:26:30 -0300

    feature: Connecting microservices

--- backend-checkout/src/services/Api.js
+++ backend-checkout/src/services/Api.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import 'dotenv/config';
 
 export default Axios.create({
-    baseURL: `${process.env.PRODUCT_URI}`,
+    baseURL: `http://${process.env.PRODUCT_URI}`,
     headers: {
         "Content-Type": "application/json"
     }
